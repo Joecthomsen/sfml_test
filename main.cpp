012932c5597cc193e5f64eeae931c5baabfe71c7
Johannes <joecthomsen@gmail.com>
2022-01-27 20:02:35 +0100

    Working sfml lib

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
 
 int main(){
 
     printf("Hello, world! from branch Johannes\n");
-    return 0;
+    
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
+
+    return 0;
 }
